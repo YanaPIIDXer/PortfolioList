3e0c814c7b3513c1b5f0c95918f674764ffc86a2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-28 23:00:58 +0900

    サブネット追加

--- Infrastructure/vpc.tf
+++ Infrastructure/vpc.tf
@@ -4,3 +4,39 @@ resource "aws_vpc" "vpc" {
         Name = "${var.name_prefix}_VPC"
     }
 }
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ap-northeast-3a"
+    tags = {
+        Name = "${var.name_prefix}_Subnet_Public"
+    }
+}
+
+resource "aws_subnet" "private1" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "ap-northeast-3a"
+    tags = {
+        Name = "${var.name_prefix}_Subnet_Private1"
+    }
+}
+
+resource "aws_subnet" "private2" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "ap-northeast-3b"
+    tags = {
+        Name = "${var.name_prefix}_Subnet_Private2"
+    }
+}
+
+resource "aws_subnet" "private3" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.4.0/24"
+    availability_zone = "ap-northeast-3c"
+    tags = {
+        Name = "${var.name_prefix}_Subnet_Private3"
+    }
+}
